0fec2c96043629a9beb3827ccf117e34126c9cdf
Lukman Hidayah <lukman.hidayah@ottodigital.id>
2021-11-22 17:18:32 +0700

    Integration with oc

--- android/app/src/main/kotlin/com/example/integratesdk/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/integratesdk/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.integratesdk
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
 import androidx.annotation.NonNull
+import com.otto.ottosdk.OttoSdk
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import com.lukman.sdklog.lib
+import org.json.JSONException
+import org.json.JSONObject
+
+//import com.lukman.sdklog.lib
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "samples.flutter.dev/log"
 
-    private val myLib: lib =lib()
+    //private val myLib: lib =lib()
+    private var android_id = ""
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        android_id = Settings.Secure.getString(
+            this.getContentResolver(),
+            Settings.Secure.ANDROID_ID
+        );
+
+    }
+
+override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
@@ -20,7 +37,33 @@ class MainActivity: FlutterActivity() {
             // TODO
             when (call.method) {
                 "sdkLog" -> {
-                    myLib.d(argument = call.argument<String>("params").toString())
+                    //myLib.d(argument = call.argument<String>("params").toString())
+                    val check_out_payment = JSONObject()
+                    try {
+                        check_out_payment.put("description", "Bayar Tagihan dari Julo")
+                        check_out_payment.put("merchantId", "6346354")
+                        check_out_payment.put("merchantName", "Kantin Buah")
+                        check_out_payment.put("requestDate", "2021-02-16 09:47:37")
+                        check_out_payment.put("amount", 65000)
+                        check_out_payment.put("fee", 0)
+                        check_out_payment.put("total", 65000)
+                    } catch (e: JSONException) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace()
+                    }
+
+                    val jsonStr = check_out_payment.toString()
+
+                    OttoSdk.runAtProduction().initSdk(
+                        this,
+                        "BOGASARI",
+                        android_id,
+                        "081351869149",
+                        "",
+                        jsonStr,
+                        "WaqRwEWpGnLdhRVFJtAMTreKDtMa", "94y4z78uOkcTmgddgFaYoCiAX9ga"
+                    ).start()
+
                     result.success(true)
                 }
                 else -> {
